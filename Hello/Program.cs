12c9e4348e861189198484a3b0433313a4b70462
Hiroyuki Aihara <aihara@oji-keisoku.co.jp>
2019-03-25 10:31:41 +0900

    World -> OSI

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,7 +10,7 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello OSI!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
