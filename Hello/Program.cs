17b92236b2c427b686da184fd0efac4cbbb188f1
Hiroyuki Aihara <aihara@oji-keisoku.co.jp>
2019-03-25 11:00:38 +0900

    Hello World Add
    Hello Osi after Hello World Row Added

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello OSI!");
+	    Console.WriteLine("Hello World!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
