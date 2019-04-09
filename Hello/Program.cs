77fb29c9de5a8cf0ca5ae42f8156d9961e6e935a
izumikd <izumi@kyotodensoku.co.jp>
2019-04-09 18:09:13 +0900

    Ver.1.00 push test2 2019/04/09
    ・連続push test2
      ・Step2表示削除
      ・Step4～6表示追加。

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -14,8 +14,11 @@ namespace Hello
 	        Console.WriteLine("Hello World!");
 
             Console.WriteLine("2019/04/09 Step1!");
-            Console.WriteLine("2019/04/09 Step2!");
             Console.WriteLine("2019/04/09 Step3!");
+
+            Console.WriteLine("2019/04/09 Step4!");
+            Console.WriteLine("2019/04/09 Step5!");
+            Console.WriteLine("2019/04/09 Step6!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("何かキーを押してください");
